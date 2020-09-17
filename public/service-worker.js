4f1240ddcd56a124497cbc0694bb887f8ec735b8
Hema <khemalatha507@gmail.com>
2020-09-17 16:01:36 -0400

    created manifest and fixed bugs in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,19 @@
 console.log("Hello from your SERVICE-WORKER.JS file! :) ");
 
 const FILES_TO_CACHE = [
-    '/',
+  '/',
     '/index.html',
-   
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/index.js',
+    '/styles.css',
+    
   ];
   
   const PRECACHE = 'precache-v1';
   const RUNTIME = 'runtime';
   
-  self.addEventListener('install', function(event) => {
+  self.addEventListener('install', (event) => {
     event.waitUntil(
       caches
         .open(PRECACHE)
